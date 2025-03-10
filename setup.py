83d9323599b19ccb0e91326d4ece3762d9acca8f
mighty mega <123591644+mightymega@users.noreply.github.com>
2025-03-10 10:31:48 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TermGUI",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="A simple terminal GUI library for Python.",
+    author="Brave",
+    license="MIT"
+)
